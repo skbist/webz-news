8e5c25f3fef84b9b4c60315c6c53f72292dd0740
Sandeep Bist <sandeepbist@Sandeeps-MacBook-Pro.local>
2025-01-09 12:17:01 +0545

    add orm config and related dependencies

---
+++ src/configs/db/orm.config.ts
@@ -0,0 +1,32 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const typeOrmModuleOptions: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: 'SLMetaverse',
+  entities: [__dirname + '/../../**/entities/*.entity.{ts,js}'],
+  /* Note : it is unsafe to use synchronize: true for schema synchronization
+    on production once you get data in your database. */
+  // commented synchronize true as it gives nullable error
+  synchronize: false,
+  autoLoadEntities: true,
+  migrationsRun: false,
+  migrations: [__dirname + '../migrations/*.{ts,js}'],
+  subscribers: [],
+};
+
+export const OrmConfig = {
+  ...typeOrmModuleOptions,
+  migrationsTableName: 'migrations',
+  migrations: ['src/db/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+};
+const dataSource = new DataSource(OrmConfig as DataSourceOptions);
+export default dataSource;
