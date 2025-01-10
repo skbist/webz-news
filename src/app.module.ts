51b3342fa911c167c4a15a0f9f1878a3a074f53e
Sandeep Bist <sandeepbist@Sandeeps-MacBook-Pro.local>
2025-01-10 06:57:26 +0545

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { WebzModule } from './modules/webz/webz.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Makes the config available globally
     }),
+    WebzModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
