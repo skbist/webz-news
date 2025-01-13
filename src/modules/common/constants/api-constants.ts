bd3c46f2c2d269ce70f3cea7190b6769cb3a5321
Sandeep Bist <sandeepbist@Sandeeps-MacBook-Pro.local>
2025-01-13 06:47:06 +0545

    add default page and page size as globl constants

--- src/modules/common/constants/api-constants.ts
+++ src/modules/common/constants/api-constants.ts
@@ -1,1 +1,3 @@
-export const WEBZ_BASE_URL = 'https://api.webz.io/';
+export const WEBZ_BASE_URL: string = 'https://api.webz.io/';
+export const DEFAULT_PAGE: number = 1;
+export const DEFAULT_PAGE_SIZE: number = 10;
