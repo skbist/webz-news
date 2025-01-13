016e922f219c7a1615c9b0fe63aca51dab99deac
Sandeep Bist <sandeepbist@Sandeeps-MacBook-Pro.local>
2025-01-13 20:24:30 +0545

    add typeorm module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PostIntegrationModule } from './modules/post-integration/post.integration.module';
 import { PostsModule } from './modules/posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from './config/db/orm.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        ...typeOrmModuleOptions,
+        logging: process.env.TYPEORM_LOGGING === 'true',
+      }),
+    }),
     ConfigModule.forRoot({
-      isGlobal: true, // Makes the config available globally
+      isGlobal: true,
+      load: [configuration],
     }),
     PostIntegrationModule,
     PostsModule,
